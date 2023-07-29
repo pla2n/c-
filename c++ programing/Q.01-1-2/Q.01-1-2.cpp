b8d2f80e1c5b9820a1c87c43cc46d0af13bf5341
Lee Seyong <rred5899@gmail.com>
2023-07-29 16:15:21 +0900

    practice

---
+++ c++ programing/Q.01-1-2/Q.01-1-2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main(void)
+{
+	char name[100];
+	char number[100];
+
+	std::cout << "이름: ";
+	std::cin >> name;
+
+	std::cout << "전화번호: ";
+	std::cin >> number;
+
+	std::cout << name << std::endl;
+	std::cout << number;
+	return 0;
+}
---
+++ c++ programing/Q.01-1-3/Q.01-1-3.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+int main()
+{
+    int number;
+    std::cout << "구구단을 외자! 숫자 하나를 입력 해 주세요!";
+    std::cin >> number;
+
+    for (int i = 1; i < 10; i++)
+        std::cout << i * number << std::endl;
+}
