4a76ccae5ae3dce4bfae3b83d85eb2f863ea8912
Lee Seyong <rred5899@gmail.com>
2023-07-29 15:37:05 +0900

    c++ practice

---
+++ c++ programing/hello wrld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(void)
+{
+	int num = 20;
+	std::cout << "Hello World!" << std::end1;
+	std::cout << "Hello " << "World!" << std::end1;
+	std::cout << num << ' ' << 'A';
+	std::cout << ' ' << 3.14 << std::end1;
+	return 0;
+}
