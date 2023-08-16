be50f3138b447a0d56ea03e3b066be46835aa667
Lee Seyong <rred5899@gmail.com>
2023-08-16 11:33:22 +0900

    practice

---
+++ c++ programing/Q04-1/Q04-1.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+using namespace std;
+
+class FruitSeller
+{
+private:
+    int APPLE_PRICE;
+    int numOfApples;
+    int myMoney;
+public:
+    void InitMembers(int price, int num, int money)
+    {
+        APPLE_PRICE = price;
+        numOfApples = num;
+        myMoney = money;
+    }
+    int SaleApples(int money)
+    {
+        int num = money / APPLE_PRICE;
+        numOfApples -= num;
+        myMoney += money;
+        return num;
+    }
+    void ShowSalesResult() const
+    {
+        cout << "남은 사과: " << numOfApples << endl;
+        cout << "판매 수익:" << myMoney << endl << endl;
+    }
+};
+
+class FruitBuyer
+{
+    int myMoney;
+    int numOfApples;
+
+public:
+    void InitMembers(int money)
+    {
+        myMoney = money;
+        numOfApples = 0;
+    }
+    void BuyApples(FruitSeller& seller, int money)
+    {
+        if (money < 0)
+        {
+            cout << "잘못된 값 입력" << endl;
+            return;
+        }
+        numOfApples += seller.SaleApples(money);
+        myMoney -= money;
+    }
+
+
+int main()
+{
+    return 0;
+}
---
+++ c++ programing/Q04-2/Q04-2.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+using std::cout;
+using std::endl;
+
+class Point
+{
+private:
+    int xpos, ypos;
+public:
+    Point(int x, int y) : xpos(x), ypos(y)
+    { }
+    void ShowPointInfo() const
+    {
+        cout << "[" << xpos << "," << ypos << "]" << endl;
+    }
+};
+
+class Circle
+{
+private:
+    int radious;
+    Point center;
+public:
+    Circle(int x, int y, int r) : center(x, y)
+    {
+        radious = r;
+    }
+    void ShowCircleInfo() const
+    {
+        cout << "radius:" << radious << endl;
+        center.ShowPointInfo();
+    }
+};
+
+class Ring
+{
+private:
+    Circle incircle;
+    Circle outcircle;
+public:
+    Ring(int x1, int y1, int r1, int x2, int y2, int r2) :incircle(x1, y1, r1), outcircle(x2, y2, r2)
+    { }
+    void ShowRingInfo() const
+    {
+        cout << "Inner Circle Info..." << endl;
+        incircle.ShowCircleInfo();
+        cout << "Outter Circle Info..." << endl;
+        outcircle.ShowCircleInfo();
+    }
+};
+
+int main()
+{
+    Ring ring(1, 1, 4, 2, 2, 9);
+    ring.ShowRingInfo();
+    return 0;
+}
